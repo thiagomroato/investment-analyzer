6993067b535b8eb9c57ad1b330d3ed27c420f387
thiagomroato <65723256+thiagomroato@users.noreply.github.com>
2026-04-01 15:40:59 -0400

    Criar assets.js

---
+++ assets.js
@@ -0,0 +1,16 @@
+const assets = [
+  // BR
+  { name: "Petrobras", ticker: "PETR4", sector: "Energia" },
+  { name: "Vale", ticker: "VALE3", sector: "Mineração" },
+  { name: "Itaú", ticker: "ITUB4", sector: "Financeiro" },
+  { name: "Banco do Brasil", ticker: "BBAS3", sector: "Financeiro" },
+  { name: "WEG", ticker: "WEGE3", sector: "Industrial" },
+  { name: "Ambev", ticker: "ABEV3", sector: "Consumo" },
+
+  // EUA
+  { name: "Apple", ticker: "AAPL", sector: "Tecnologia" },
+  { name: "Microsoft", ticker: "MSFT", sector: "Tecnologia" },
+  { name: "Google", ticker: "GOOGL", sector: "Tecnologia" },
+  { name: "Amazon", ticker: "AMZN", sector: "Consumo" },
+  { name: "Coca-Cola", ticker: "KO", sector: "Consumo" },
+];

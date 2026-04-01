d6a37d5405cd0e8aba4054ecb433fbb90ef1e927
thiagomroato <65723256+thiagomroato@users.noreply.github.com>
2026-04-01 15:25:36 -0400

    Excluir main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from comparator import analyze_stock
-
-def main():
-    print("=== ANALISADOR DE AÇÕES ===")
-    ticker = input("Digite o ticker (ex: PETR4): ").upper()
-    analyze_stock(ticker)
-
-if __name__ == "__main__":
-    main()

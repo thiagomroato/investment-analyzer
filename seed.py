c613283f31f1683d2c7bf7500fd858b6fe948747
thiagomroato <65723256+thiagomroato@users.noreply.github.com>
2026-04-01 15:26:04 -0400

    Criar seed.py

---
+++ seed.py
@@ -0,0 +1,36 @@
+from models import connect
+
+def seed_assets():
+    conn = connect()
+    cursor = conn.cursor()
+
+    assets = [
+        # BRASIL
+        ("Petrobras", "PETR4", "Energia", "BR"),
+        ("Vale", "VALE3", "Mineração", "BR"),
+        ("Itaú", "ITUB4", "Financeiro", "BR"),
+        ("Banco do Brasil", "BBAS3", "Financeiro", "BR"),
+        ("WEG", "WEGE3", "Industrial", "BR"),
+        ("Ambev", "ABEV3", "Consumo", "BR"),
+        ("Taesa", "TAEE11", "Energia", "BR"),
+        ("BB Seguridade", "BBSE3", "Financeiro", "BR"),
+
+        # EUA
+        ("Apple", "AAPL", "Tecnologia", "US"),
+        ("Microsoft", "MSFT", "Tecnologia", "US"),
+        ("Google", "GOOGL", "Tecnologia", "US"),
+        ("Amazon", "AMZN", "Consumo", "US"),
+        ("Coca-Cola", "KO", "Consumo", "US"),
+        ("Johnson & Johnson", "JNJ", "Saúde", "US"),
+        ("Berkshire Hathaway", "BRK.B", "Financeiro", "US"),
+        ("Tesla", "TSLA", "Automotivo", "US"),
+    ]
+
+    for asset in assets:
+        cursor.execute("""
+        INSERT INTO assets (name, ticker, sector, market)
+        VALUES (?, ?, ?, ?)
+        """, asset)
+
+    conn.commit()
+    conn.close()
